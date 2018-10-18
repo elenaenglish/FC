b13b694dd44aaa36dcbaa55d33c65e2b3ed531ca
elenaenglish <eenglish@pijnz.com>
2018-10-18 16:11:47 -0700

    push application

---
+++ WebApplication2/WebApplication2/App_Start/RouteConfig.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication2
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+             "Main",                                           // Route name
+             "",                            // URL with parameters
+             new { controller = "Main", action = "Index" }  // Parameter defaults
+         );
+            routes.MapRoute(
+             "Platform",                                           // Route name
+             "Platform",                            // URL with parameters
+             new { controller = "Main", action = "Platform" }  // Parameter defaults
+         );
+            routes.MapRoute(
+             "Token-Sale",                                           // Route name
+             "token-sale",                            // URL with parameters
+             new { controller = "Main", action = "TokenSale" }  // Parameter defaults
+         );
+            routes.MapRoute(
+             "Partners",                                           // Route name
+             "Partners",                            // URL with parameters
+             new { controller = "Main", action = "Partners" }  // Parameter defaults
+         );
+            routes.MapRoute(
+             "Community",                                           // Route name
+             "Community",                            // URL with parameters
+             new { controller = "Main", action = "Community" }  // Parameter defaults
+         );
+            routes.MapRoute(
+             "Academy",                                           // Route name
+             "forum-academy",                            // URL with parameters
+             new { controller = "Main", action = "ForumAcademy" }  // Parameter defaults
+         );
+
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Controllers/MainController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+   
+
+    public class MainController : Controller    
+    {
+
+        private string navStyleHome = " navbar navbar-expand-lg navbar-dark fixed-top ";
+        private string navStyle = " navbar navbar-expand-lg navbar-dark fixed-top navbar-inner-page ";
+
+
+        public ActionResult Index()
+        {
+            ViewData["pageTitle"] = "Forum Cooperative";
+            ViewData["navStyle"] = navStyleHome;
+            return View("Index");
+        }
+        public ActionResult Platform()
+        {
+            ViewData["pageTitle"] = "Forum Cooperative : Platform";
+            ViewData["navStyle"] = navStyle;
+            return View("Platform");
+        }
+        public ActionResult Partners()
+        {
+            ViewData["pageTitle"] = "Forum Cooperative : Partners";
+            ViewData["navStyle"] = navStyle;
+            return View("Partners");
+        }
+        public ActionResult TokenSale()
+        {
+            ViewData["pageTitle"] = "Forum Cooperative : Token Sale";
+            ViewData["navStyle"] = navStyle;
+            return View("TokenSale");
+        }
+        public ActionResult Community()
+        {
+            ViewData["pageTitle"] = "Forum Cooperative : Community";
+            ViewData["navStyle"] = navStyle;
+            return View("Community");
+        }
+        public ActionResult ForumAcademy()
+        {
+            ViewData["pageTitle"] = "Forum Cooperative : Forum Academy";
+            ViewData["navStyle"] = navStyle;
+            return View("ForumAcademy");
+        }
+    }
+   
+}
